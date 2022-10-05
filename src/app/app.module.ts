266270a5659dc1ce1d902de09cdfc84cfe5814e0
Rojer19bcs018 <101931464+Rojer19bcs018@users.noreply.github.com>
2022-10-05 22:14:51 +0530

    3 files modified, 2 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { HeaderClass } from '../header/header.component';
 import { CourseClass } from '../course/course.component';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { CourseDetails } from '../coursedetails/coursedetails.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, HeaderClass, CourseClass],
+  imports: [BrowserModule, FormsModule, 
+    RouterModule.forRoot([
+      {path:'',component:CourseClass},
+      {path:'course/:courseid',component:CourseDetails}
+    ])],
+  declarations: [AppComponent, HelloComponent, HeaderClass, CourseClass, CourseDetails],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/coursedetails/coursedetails.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'app-course-details',
+  templateUrl:'./coursedetails.component.html'
+})
+
+export class CourseDetails{
+
+}
