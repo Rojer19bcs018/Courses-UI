0769fa6f8986960bb43cf5e7f13a90b7b2beded4
Rojer19bcs018 <101931464+Rojer19bcs018@users.noreply.github.com>
2022-10-08 17:30:19 +0530

    5 files modified

--- src/course/course.model.ts
+++ src/course/course.model.ts
@@ -1,6 +1,7 @@
 export interface course {
   id: number;
   name: string;
+  price: number;
   description: string;
 }
 
@@ -8,16 +9,19 @@ export const courses = [
   {
     id: 1,
     name: 'Maths',
+    price: 567,
     description: 'This is good to learn',
   },
   {
     id: 2,
     name: 'Science',
+    price: 674,
     description: 'This is good for your life',
   },
   {
     id: 3,
     name: 'Social',
+    price: 852,
     description: 'This is good for society',
   },
 ];
--- src/coursedetails/coursedetails.component.ts
+++ src/coursedetails/coursedetails.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { courses,course } from '../course/course.model';
 
 @Component({
   selector:'app-course-details',
   templateUrl:'./coursedetails.component.html'
 })
 
-export class CourseDetails{
+export class CourseDetails implements OnInit{
+    Course:course | undefined;
+    constructor(private route:ActivatedRoute){}
+      ngOnInit(){
+           const routeParams = this.route.snapshot.paramMap;
+           const fromRoute = Number(routeParams.get('courseid'));
 
+           this.Course = courses.find(Course => Course.id === fromRoute);
+      }
+    
 }
