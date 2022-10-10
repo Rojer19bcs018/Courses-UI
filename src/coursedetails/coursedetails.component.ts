c6cf119c743c9e221f94af15f0dc0dd3430d80c5
Rojer19bcs018 <101931464+Rojer19bcs018@users.noreply.github.com>
2022-10-10 21:51:26 +0530

    2 files modified

--- src/coursedetails/coursedetails.component.ts
+++ src/coursedetails/coursedetails.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { courses,course } from '../course/course.model';
+import { CartService } from '../app/cart.service';
 
 @Component({
   selector:'app-course-details',
@@ -9,7 +10,13 @@ import { courses,course } from '../course/course.model';
 
 export class CourseDetails implements OnInit{
     Course:course | undefined;
-    constructor(private route:ActivatedRoute){}
+    constructor(
+      private route:ActivatedRoute,
+      private cartService: CartService){}
+      addToCart(Course: course) {
+        this.cartService.addToCart(Course);
+        window.alert('Your product has been added to the cart!');
+      }
       ngOnInit(){
            const routeParams = this.route.snapshot.paramMap;
            const fromRoute = Number(routeParams.get('courseid'));
